4621ebc510a45dc760a32623fa9d5f859ad1a6ea
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-04-01 16:13:20 +0900

    Chore: 프로젝트 기초세팅

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,13 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+:root {
+  /* color */
+  --color-main: #76dd4e;
+  --color-black: #222;
+  --color-white: #fff;
+  --color-warning: #ff9191;
+  --color-grey: #d9d9d9;
+}
   body {
-    position: relative;
     background-color: #fff;
     margin: 0 auto;
   }
-  button{
-    cursor: pointer;
-  }
+
 `;
 export default GlobalStyles;
---
+++ src/components/modal/ModalLayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ModalLayout() {
+  return <div>ModalLayout</div>;
+}
+
+export default ModalLayout;
