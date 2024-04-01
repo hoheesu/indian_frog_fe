a37a68d95f383e3517cffbeec93f1731ba55db99
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-04-01 15:13:11 +0900

    Chore: 리액트 초기세팅

---
+++ src/assets/styles/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    position: relative;
+    background-color: #fff;
+    margin: 0 auto;
+  }
+  button{
+    cursor: pointer;
+  }
+`;
+export default GlobalStyles;
