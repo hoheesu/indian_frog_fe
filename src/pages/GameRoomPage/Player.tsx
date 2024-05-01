e9058f636ca87641f4e4547e422670f982a9b72b
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-05-01 23:23:30 +0900

    Fix: 게임룸 ui 수정

--- src/pages/GameRoomPage/Player.tsx
+++ src/pages/GameRoomPage/Player.tsx
@@ -53,7 +53,7 @@ const Player: React.FC<PlayerProps> = ({ player, nick, point, state }) => {
               </PlayerInfo>
             </TopArea>
             <BottomArea>
-              <Timer>00:00</Timer>
+              {/* <Timer>00:00</Timer> */}
               <Status>
                 {state === 'ready'
                   ? 'Ready'
@@ -146,14 +146,12 @@ const PlayerProfile = withPlayer(styled.div<WithPlayerProps>`
   justify-content: center;
   align-items: center;
   border-radius: 50%;
-  background: ${({ player }) =>
-    player === 'other'
-      ? 'conic-gradient(#8dd012 277deg, #e3d5b3 0deg)'
-      : 'conic-gradient(#CD7522 277deg, #e3d5b3 0deg)'};
+  border: ${({ player }) =>
+    player === 'other' ? ' 3px solid #8dd012' : '3px solid #CD7522'};
   overflow: hidden;
   img {
-    width: 100px;
-    height: 100px;
+    width: 120px;
+    height: 120px;
     object-fit: fill;
     background: #fff;
     border-radius: 50%;
